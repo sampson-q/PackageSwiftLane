47c7cf3f41307d00d1c32fb16919ff8ae42ef59c
sampson-q <quarmysokpoli@gmail.com>
2026-03-09 15:34:22 +0000

    fis: Fatal error:  Uncaught TypeError: Unsupported operand types: string + int on line 514

--- lib/Core.php
+++ lib/Core.php
@@ -413,7 +413,7 @@ class Core
   public function cdp_getShipmode()
   {
     // Use categories table as shipping modes
-    $sql = "SELECT id, name_item AS ship_mode FROM cdb_category ORDER BY id ASC";
+    $sql = "SELECT id, name_item AS ship_mode FROM cdb_shipping_mode ORDER BY id ASC";
 
     $this->db->cdp_query($sql);
     $this->db->cdp_execute();
@@ -511,7 +511,7 @@ class Core
     $invNum = $this->db->cdp_fetch_assoc();
     $max_id = $invNum['order_no'];
     $cod = $max_id;
-    $sig = $cod + 1;
+    $sig = (int) $cod + 1;
 
     $Strsig = (string)$sig;
     $formato = str_pad($Strsig, "" . $digitss . "", "0", STR_PAD_LEFT);
