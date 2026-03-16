483aab4e05cce7a5ec66dd55ca2daea2ff930ef4
sampson-q <quarmysokpoli@gmail.com>
2026-03-16 17:44:52 +0000

    feat: push notification for consolidation

---
+++ push_notifications_consolidation.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("loader.php");
+
+    $user = new User();
+    $core = new Core();
+    if ($user->cdp_loginCheck() == true) {
+
+        $permissions = $user->cdp_getUserPermissions();
+
+        if (!$user->cdp_hasPermission('push_notifications')) {
+            header("location: error403.php");
+            exit;
+        }
+       
+        include('views/tools/push_notifications_consolidation.php');
+           
+    } else{
+        header("location: login.php");
+        exit;       
+    }
+?>
