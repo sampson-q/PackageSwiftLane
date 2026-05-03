5016b0de8057bf41a48926f5fe840cd845d5f2e1
sampson-q <quarmysokpoli@gmail.com>
2026-05-03 02:04:46 +0000

    update: csrf protection implementation

--- views/courier/courier_edit.php
+++ views/courier/courier_edit.php
@@ -141,7 +141,7 @@ $numrows     = $db->cdp_rowCount();
             </div>
 
             <form method="post" id="invoice_form" name="invoice_form" enctype="multipart/form-data">
-
+                <input type="hidden" name="_csrf_token" value="<?php echo htmlspecialchars(cdp_csrf_token(), ENT_QUOTES, 'UTF-8'); ?>">
                 <div class="container-fluid">
 
                     <!-- 1) Datos del envío -->
