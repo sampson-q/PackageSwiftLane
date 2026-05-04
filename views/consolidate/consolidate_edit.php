a1a7b6961136e6e132ef3e6922e47e2ecdfcb4b9
sampson-q <quarmysokpoli@gmail.com>
2026-05-04 00:00:05 +0000

    fix: recipient info

--- views/consolidate/consolidate_edit.php
+++ views/consolidate/consolidate_edit.php
@@ -58,7 +58,12 @@ $order_items = $db->cdp_registros();
 $db->cdp_query("SELECT * FROM cdb_users where id= '" . $row_order->sender_id . "'");
 $sender_data = $db->cdp_registro();
 
-$db->cdp_query("SELECT * FROM cdb_recipients where id= '" . $row_order->receiver_id . "'");
+if ($row_order->recipient_type == 'user') {
+    $db->cdp_query("SELECT * FROM cdb_users where id= '" . $row_order->receiver_id . "'");
+} else {
+    $db->cdp_query("SELECT * FROM cdb_recipients where id= '" . $row_order->receiver_id . "'");
+}
+
 $receiver_data = $db->cdp_registro();
 
 
