40e607143c71d4ee76404ec683de3d3e9d4053fa
Sampson Quarmy <quarmysokpoli@gmail.com>
2026-05-14 14:25:31 +0000

    update: package consolidate to match normal consolidation

--- dataJs/consolidate_package_add.js
+++ dataJs/consolidate_package_add.js
@@ -444,48 +444,6 @@ $(document).ready(function () {
       return false;
     }
 
-    if ($.trim($("#recipient_id").val()).length == 0) {
-      Swal.fire({
-        title: "Oops...",
-        text: message_error_form86,
-        icon: "error",
-        confirmButtonColor: "#336aea",
-      });
-      return false;
-    }
-
-    if ($.trim($("#recipient_address_id").val()).length == 0) {
-      Swal.fire({
-        title: "Oops...",
-        text: message_error_form87,
-        icon: "error",
-        confirmButtonColor: "#336aea",
-      });
-      return false;
-    }
-
-    //data sender
-
-    if ($.trim($("#sender_id").val()).length == 0) {
-      Swal.fire({
-        title: "Oops...",
-        text: message_error_form88,
-        icon: "error",
-        confirmButtonColor: "#336aea",
-      });
-      return false;
-    }
-
-    if ($.trim($("#sender_address_id").val()).length == 0) {
-      Swal.fire({
-        title: "Oops...",
-        text: message_error_form89,
-        icon: "error",
-        confirmButtonColor: "#336aea",
-      });
-      return false;
-    }
-
     if ($.trim($("#driver_id").val()) == 0) {
       Swal.fire({
         title: "Oops...",
