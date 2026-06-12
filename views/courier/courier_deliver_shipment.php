4a21a75477031a136db1ee7bf068e336b6f00705
Sampson Quarmy <quarmysokpoli@gmail.com>
2026-06-12 10:56:27 +0000

    fix(courier-deliver): remove receiver WhatsApp notify checkbox
    Receiver notifications are sender-only. Remove the notify_whatsapp_receiver checkbox.

--- views/courier/courier_deliver_shipment.php
+++ views/courier/courier_deliver_shipment.php
@@ -276,12 +276,6 @@ $driverrow = $user->cdp_userAllDriver();
                                                     <span class="custom-control-indicator"></span>
                                                 </label>
 
-                                                <label class="custom-control custom-checkbox" style="font-size: 18px; padding-left: 0px">
-                                                    <input type="checkbox" class="custom-control-input" name="notify_whatsapp_receiver" id="notify_whatsapp_receiver" value="1" checked>
-                                                    <b><?php echo $lang['leftorder144442'] ?> <i class="mdi mdi-whatsapp" style="font-size: 22px; color:#07bc4c;"></i></b>
-                                                    <span class="custom-control-indicator"></span>
-                                                </label>
-
                                             </div>
                                         <?php } ?>
 
