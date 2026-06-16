38cefba71d162b14f4adfd620d0e0c1fc296defb
Sampson Quarmy <quarmysokpoli@gmail.com>
2026-06-16 23:01:00 +0000

    feat(consolidate-modal): widen to 92 vw, add status filter dropdown and per-page selector
    Change modal-lg to modal-xl with max-width/width: 92vw and modal-dialog-scrollable
    so the package table has enough room. Narrow the search input column to col-md-5.
    
    Add a status filter dropdown (#fs_filter_status, col-md-4) built from
    ->cdp_getStatus() excluding statuses 8, 14, and 21. Add a per-page selector
    (#fs_per_page, col-md-3) with options 50 and 100. Both trigger cdp_load(1) on
    change and are read by the updated consolidate_add/edit.js.

--- views/modals/modal_add_ship_consolidate.php
+++ views/modals/modal_add_ship_consolidate.php
@@ -1,6 +1,6 @@
 	<!-- Modal -->
 	<div class="modal fade" id="myModalConsolidate" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-		<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-dialog modal-xl modal-dialog-scrollable" role="document" style="max-width: 92vw; width: 92vw;">
 			<div class="modal-content">
 				<div class="modal-header">
 					<h4 class="modal-title" id="myModalLabel"><i class='glyphicon glyphicon-envelope'></i> <?php echo $lang['leftorder148']; ?>
@@ -13,7 +13,7 @@
 
 						<div class="row mb-3 ml-2">
 
-							<div class=" col-sm-12 col-md-6">
+							<div class="col-sm-12 col-md-5">
 
 								<div class="input-group">
 									<input type="text" name="search" id="search" class="form-control input-sm float-right" placeholder="<?php echo $lang['left21551'] ?>" onkeyup="cdp_load(1);">
@@ -22,6 +22,27 @@
 									</div>
 
 								</div>
+							</div><!-- /.col -->
+
+							<div class="col-sm-6 col-md-4">
+								<select id="fs_filter_status" class="form-control form-control-sm" onchange="cdp_load(1);">
+									<option value="0"><?php echo 'All package statuses'; ?></option>
+									<?php
+										$fs_status_list = method_exists($core, 'cdp_getStatus') ? $core->cdp_getStatus() : array();
+										foreach ($fs_status_list as $fs_st) {
+											// Skip statuses the modal already excludes (pickup/delivered/cancelled).
+											if (in_array((int)$fs_st->id, [8, 14, 21])) continue;
+											echo '<option value="' . (int)$fs_st->id . '">' . htmlspecialchars($fs_st->mod_style) . '</option>';
+										}
+									?>
+								</select>
+							</div><!-- /.col -->
+
+							<div class="col-sm-6 col-md-3">
+								<select id="fs_per_page" class="form-control form-control-sm" onchange="cdp_load(1);">
+									<option value="50">50 rows</option>
+									<option value="100">100 rows</option>
+								</select>
 							</div><!-- /.col -->
 						</div>
 
