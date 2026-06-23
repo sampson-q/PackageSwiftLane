0d65d993faaff95588c55ca6f978c4a1c955b1e1
Sampson Quarmy <quarmysokpoli@gmail.com>
2026-06-23 09:03:07 +0000

    fix(customers-edit): add CSRF token hidden input to the edit_user form

--- views/customers/customers_edit.php
+++ views/customers/customers_edit.php
@@ -331,6 +331,7 @@ $history = $db->cdp_registros();
                                         <div class="card-body">
 
                                             <form enctype="multipart/form-data" class="form-horizontal form-material" id="edit_user" name="edit_user" method="post">
+                                                <input type="hidden" name="_csrf_token" value="<?php echo htmlspecialchars(cdp_csrf_token(), ENT_QUOTES, 'UTF-8'); ?>">
                                                 <section>
                                                     <?php if ($userData->userlevel == 9 || $userData->userlevel == 2) { ?>
                                                         <div class="row">
